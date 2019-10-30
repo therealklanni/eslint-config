b79b690e5df4a9b9990e06f2db9b422b60ac9ca8
Kevin Lanni <therealklanni@gmail.com>
2019-10-29 17:49:28 -0700

    feat(node): add rules

--- node.js
+++ node.js
@@ -1,5 +1,15 @@
 module.exports = {
   env: { node: true, browser: false },
   extends: ['./base'],
-  rules: {}
+  rules: {
+    'no-console': 'off',
+    'global-require': 'error',
+    'no-buffer-constructor': 'error',
+    'no-mixed-requires': 'error',
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-process-env': 'warn',
+    'no-process-exit': 'warn',
+    'no-sync': 'error'
+  }
 }
