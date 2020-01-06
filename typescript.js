04b59a042968c6d935b69aebda23072d68609289
Kevin Lanni <therealklanni@gmail.com>
2020-01-06 12:50:12 -0700

    feat: add typescript configs

---
+++ typescript.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-magic-numbers */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    './base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'error',
+    // must disable the base rule as it can report incorrect errors
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    // must disable the base rule as it can report incorrect errors
+    'func-call-spacing': 'off',
+    '@typescript-eslint/func-call-spacing': 'error',
+    // must disable the base rule as it can report incorrect errors
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-extra-non-null-assertion': 'error',
+    // must disable the base rule as it can report incorrect errors
+    'no-extra-parens': 'off',
+    '@typescript-eslint/no-extra-parens': ['error'],
+    // must disable the base rule as it can report incorrect errors
+    'no-extra-semi': 'off',
+    '@typescript-eslint/no-extra-semi': ['error'],
+    '@typescript-eslint/no-extraneous-class': 'error',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-implied-eval': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+    '@typescript-eslint/no-untyped-public-signature': 'error',
+    // must disable the base rule as it can report incorrect errors
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': ['error'],
+    // must disable the base rule as it can report incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }
+    ],
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    '@typescript-eslint/prefer-regexp-exec': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    // must disable the base rule as it can report incorrect errors
+    quotes: 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/return-await': 'error',
+    // must disable the base rule as it can report incorrect errors
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    '@typescript-eslint/unified-signatures': 'error'
+  }
+}
